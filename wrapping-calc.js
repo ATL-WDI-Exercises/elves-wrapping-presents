8a14357aa7a9ca48667db40e913a86d4597ca39b
Jamie King <king0120@gmail.com>
2015-12-07 16:31:26 -0500

    Finished first question

--- wrapping-calc.js
+++ wrapping-calc.js
@@ -7,7 +7,34 @@
 */
 function wrappingCalculator(boxes) {
   // TODO: return the correct amount of wrapping paper needed.
-  return 0;
-};
+  var boxDimensions = boxes.split('x');
+  var length = boxDimensions[0];
+  var width = boxDimensions[1];
+  var height = boxDimensions[2];
+  console.log("Length: " + length + ". Width: " + width + ". Height: "+height);
+
+  var lengthHeight = length * height;
+  var lengthWidth = length * width;
+  var widthHeight = width * height;
+  console.log("lengthheight: "+lengthHeight+" lengthWidth: "+lengthWidth+" widthHeight: "+widthHeight)
+
+  var surfaceArea = 2 * lengthHeight + 2 * lengthWidth + 2 * widthHeight;
+  var extraPaper = 0;
+  if (lengthHeight < lengthWidth && lengthHeight < widthHeight){
+    extraPaper = lengthHeight;
+  } else if (lengthWidth < lengthHeight && lengthWidth < widthHeight){
+    extraPaper = lengthWidth;
+  } else {
+    extraPaper = widthHeight;
+  }
+  console.log("surfaceArea: " + surfaceArea);
+
+  console.log("extraPaper: " + extraPaper);
+  return surfaceArea + extraPaper;
+}
+
+var paper = wrappingCalculator(['2x3x4', '1x1x10']);
+
+console.log('paper needed: '+ paper);
 
 exports.wrappingCalculator = wrappingCalculator;
