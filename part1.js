2105122652137fbe53b4516643cbe47750ba40fa
Jamie King <king0120@gmail.com>
2015-12-07 17:17:18 -0500

    Finished part 1 and 2

--- part1.js
+++ part1.js
@@ -6,35 +6,45 @@
      ['2x3x4', '3x5x8', '1x1x10']
 */
 function paperCalculator(boxes) {
-  // TODO: return the correct amount of wrapping paper needed.
-  var boxDimensions = boxes.split('x');
-  var length = boxDimensions[0];
-  var width = boxDimensions[1];
-  var height = boxDimensions[2];
-  console.log("Length: " + length + ". Width: " + width + ". Height: "+height);
 
-  var lengthHeight = length * height;
-  var lengthWidth = length * width;
-  var widthHeight = width * height;
-  console.log("lengthheight: "+lengthHeight+" lengthWidth: "+lengthWidth+" widthHeight: "+widthHeight)
+  var totalPaper = 0;
 
-  var surfaceArea = 2 * lengthHeight + 2 * lengthWidth + 2 * widthHeight;
-  var extraPaper = 0;
-  if (lengthHeight < lengthWidth && lengthHeight < widthHeight){
-    extraPaper = lengthHeight;
-  } else if (lengthWidth < lengthHeight && lengthWidth < widthHeight){
-    extraPaper = lengthWidth;
-  } else {
-    extraPaper = widthHeight;
-  }
-  console.log("surfaceArea: " + surfaceArea);
+  //The forEach function will allow the user to put multiple boxes into the
+  //array and still return the correct calculation
 
-  console.log("extraPaper: " + extraPaper);
-  return surfaceArea + extraPaper;
+  boxes.forEach(function(boxString) {
+    //The first line of this function splits the string into an array of
+    //[length,width,height]
+
+    var boxDimensions = boxString.split('x');
+    var length = boxDimensions[0];
+    var width = boxDimensions[1];
+    var height = boxDimensions[2];
+
+    //These variables compute the 3 calculations needed to find the surface
+    //area.
+
+    var lengthHeight = length * height;
+    var lengthWidth = length * width;
+    var widthHeight = width * height;
+
+    var surfaceArea = 2 * lengthHeight + 2 * lengthWidth + 2 * widthHeight;
+    var extraPaper = 0;
+
+    //This if statement determines which of the following variable are the
+    //shortest. This lets the function know how much extra paper is needed.
+
+    if (lengthHeight < lengthWidth && lengthHeight < widthHeight) {
+      extraPaper = lengthHeight;
+    } else if (lengthWidth < lengthHeight && lengthWidth < widthHeight) {
+      extraPaper = lengthWidth;
+    } else {
+      extraPaper = widthHeight;
+    };
+
+    totalPaper += surfaceArea + extraPaper;
+  });
+  return totalPaper;
 }
 
-var paper = wrappingCalculator(['2x3x4', '1x1x10']);
-
-console.log('paper needed: '+ paper);
-
-exports.paperCalculator = paperCalculator;
+module.exports.paperCalculator = paperCalculator;
--- part2.js
+++ part2.js
@@ -6,7 +6,26 @@
 */
 function ribbonCalculator(boxes) {
   // TODO: return the correct amount of ribbon needed.
-  return 0;
-};
+  var totalRibbon = 0;
+
+  //This forEach function allows the user to input multiple boxes into the
+  //array and still return the correct amount of ribbon.
+  boxes.forEach(function(boxString){
+
+    var boxDimensions = boxString.split('x');
+
+    //This sort function sorts the length, height, and width by smallest to
+    //largest.  This allows the program to call on the first 2 values of the
+    //new array and always have the smallest 2 numbers for ribbonWrap.
+
+    var sortedDimensions = boxDimensions.sort(function(a, b){return a-b;});
+
+    var ribbonWrap = 2*sortedDimensions[0] + 2*sortedDimensions[1];
+    var ribbonBow = boxDimensions[0]*boxDimensions[1]*boxDimensions[2];
+
+    totalRibbon += ribbonWrap + ribbonBow;
+  });
+    return totalRibbon;
+}
 
 exports.ribbonCalculator = ribbonCalculator;
