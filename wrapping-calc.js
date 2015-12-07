905c842268773780708b89e86fde1ab3d0d0d6c5
Anthony Sims Jr <anthony.sims.226@gmail.com>
2015-12-07 16:32:56 -0500

    loose ends

--- wrapping-calc.js
+++ wrapping-calc.js
@@ -5,9 +5,15 @@
    dimensions of a box to be wrapped, for example:
      ['2x3x4', '3x5x8', '1x1x10']
 */
+var l = "";
+var w = "";
+var h = "";
+
 function wrappingCalculator(boxes) {
   // TODO: return the correct amount of wrapping paper needed.
   return 0;
 };
 
+var paperNeeded = wrappingCalculator(['2x3x4','1x1x10'])
+  console.log('You need ' + paperNeeded + 'square feet of wrapping paper.')
 exports.wrappingCalculator = wrappingCalculator;
