8768bee72c3b4bfcd9d2609d5735a7b2e06fc2f3
Steve Smith <stevewillsmith@gmail.com>
2015-12-08 18:14:16 -0500

    Steven Smith's finished submission

--- part1.js
+++ part1.js
@@ -5,9 +5,34 @@
    dimensions of a box to be wrapped, for example:
      ['2x3x4', '3x5x8', '1x1x10']
 */
-function paperCalculator(boxes) {
-  // TODO: return the correct amount of wrapping paper needed.
-  return 0;
-};
 
-exports.paperCalculator = paperCalculator;
+var boxArray = ['2x3x4', '3x5x8', '1x1x10'];
+function paperCalculator(boxesArr) {
+  var total = 0;
+  for (i = 0; i < boxesArr.length; i++) {
+    total += paperCalculatorHelper(boxesArr[i]);
+  }
+  return total;
+}
+
+function paperCalculatorHelper(boxes) {
+  var l = boxes.slice(0, boxes.indexOf('x'));
+  var w = boxes.slice(boxes.indexOf('x') + 1, boxes.lastIndexOf('x'));
+  var h = boxes.slice(boxes.lastIndexOf('x') + 1);
+
+  var parL = parseInt(l, 10);
+  var parW = parseInt(w, 10);
+  var parH = parseInt(h, 10);
+
+  var values = [(parL * parW), (parW * parH), (parH * parL)];
+  values.sort(function(a, b) {
+    return a - b;
+  });
+  var totalPaper = ((2 * values[0]) + (2 * values[1]) + (2 * values[2]) +
+  values[0]);
+  return totalPaper;
+}
+
+paperCalculator(boxArray);
+module.exports.paperCalculator = paperCalculator;
+
--- part2.js
+++ part2.js
@@ -4,9 +4,33 @@
    dimensions of a box to be wrapped, for example:
      ['2x3x4', '3x5x8', '1x1x10']
 */
-function ribbonCalculator(boxes) {
-  // TODO: return the correct amount of ribbon needed.
-  return 0;
-};
+
+var boxArray = ['2x3x4', '3x5x8', '1x1x10'];
+function ribbonCalculator(boxesArr) {
+  var total = 0;
+  for (i = 0; i < boxesArr.length; i++) {
+    total += ribbonCalculatorHelper(boxesArr[i]);
+  }
+  return total;
+}
+
+function ribbonCalculatorHelper(boxes) {
+  var l = boxes.slice(0, boxes.indexOf('x'));
+  var w = boxes.slice(boxes.indexOf('x') + 1, boxes.lastIndexOf('x'));
+  var h = boxes.slice(boxes.lastIndexOf('x') + 1);
+
+  var parL = parseInt(l, 10);
+  var parW = parseInt(w, 10);
+  var parH = parseInt(h, 10);
+
+  var shortPath = [((2 * parL) + (2 * parW)), ((2 * parW) +
+    (2 * parH)), ((2 * parH) + (2 * parL))];
+  shortPath.sort(function(a, b) {
+    return a - b;
+  });
+  var totalRibbon = shortPath[0] + (parL * parW * parH);
+  return totalRibbon;
+}
+ribbonCalculator(boxArray);
 
 exports.ribbonCalculator = ribbonCalculator;
