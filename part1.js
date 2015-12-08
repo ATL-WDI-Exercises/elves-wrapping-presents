96aa0e78d5f8b9f9a3f8cc607063042e9bf09702
Anthony Sims Jr <anthony.sims.226@gmail.com>
2015-12-08 15:57:12 -0500

    Working and/or struggling through it.

--- part1.js
+++ part1.js
@@ -5,23 +5,31 @@
    dimensions of a box to be wrapped, for example:
      ['2x3x4', '3x5x8', '1x1x10']
 */
-<<<<<<< HEAD:wrapping-calc.js
-var l = "";
-var w = "";
-var h = "";
+
+var boxes = ['2x3x4', '3x5x8', '1x1x10'];
+var dimensions = [];
 
 function wrappingCalculator(boxes) {
-=======
+  for (var i = 0; i < boxes.length; i++) {
+    dimensions.push(boxes[i].split('x').map(Number));
+
+    // var l = dimensions[i][0];
+    // var w = dimensions[i][1];
+    // var h = dimensions[i][2];
+
+  }
+
+}
+
 function paperCalculator(boxes) {
->>>>>>> a311eba9ab3e2cf9971724092f2d14551ec0f296:part1.js
+
   // TODO: return the correct amount of wrapping paper needed.
   return 0;
 };
 
-<<<<<<< HEAD:wrapping-calc.js
+
 var paperNeeded = wrappingCalculator(['2x3x4','1x1x10'])
-  console.log('You need ' + paperNeeded + 'square feet of wrapping paper.')
+console.log('You need ' + paperNeeded + ' square feet of wrapping paper.')
 exports.wrappingCalculator = wrappingCalculator;
-=======
 exports.paperCalculator = paperCalculator;
->>>>>>> a311eba9ab3e2cf9971724092f2d14551ec0f296:part1.js
+
