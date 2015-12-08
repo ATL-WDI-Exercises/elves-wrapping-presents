19445c8edf2491203d6022f380af8d3dddbf8155
Chad Christensen <chadthecoder@gmail.com>
2015-12-08 13:30:23 -0500

    finished paper and ribbon calulator

--- part2.js
+++ part2.js
@@ -6,7 +6,20 @@
 */
 function ribbonCalculator(boxes) {
   // TODO: return the correct amount of ribbon needed.
-  return 0;
+  var ribbonNeeded = 0;
+  for ( var i = 0; i < boxes.length; i++) {
+    var dimensions = boxes[i].split('x');
+    var length = parseInt(dimensions[0]);
+    var width = parseInt(dimensions[1]);
+    var height = parseInt(dimensions[2]);
+    var perimeter = (2 * length) + (2 * width);
+    // console.log("top: 2x"+length+"x"+width+" = "+ top);
+    var cubicFt = length * width * height;
+    // console.log("minArea is " + minArea);
+    ribbonNeeded += (perimeter + cubicFt);
+    // console.log("paperNeeded is " + paperNeeded);
+  }
+  return ribbonNeeded;
 };
 
 exports.ribbonCalculator = ribbonCalculator;
