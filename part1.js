50035d8bac6250b3f2c2e77de741cf98c79bdb30
Anthony Sims Jr <anthony.sims.226@gmail.com>
2015-12-08 20:05:44 -0500

    FINALLY PART ONE.

--- part1.js
+++ part1.js
@@ -1,35 +1,27 @@
-/*
-   Return the wrapping paper needed to wrap a box, including some additional
-   paper needed to wrap the present (based on the smallest side of the box).
-   The boxes argument is assumed to be an array of strings each containing the
-   dimensions of a box to be wrapped, for example:
-     ['2x3x4', '3x5x8', '1x1x10']
-*/
-
-var boxes = ['2x3x4', '3x5x8', '1x1x10'];
-var dimensions = [];
-
-function wrappingCalculator(boxes) {
-  for (var i = 0; i < boxes.length; i++) {
-    dimensions.push(boxes[i].split('x').map(Number));
-
-    // var l = dimensions[i][0];
-    // var w = dimensions[i][1];
-    // var h = dimensions[i][2];
-
-  }
-
-}
-
 function paperCalculator(boxes) {
-
-  // TODO: return the correct amount of wrapping paper needed.
-  return 0;
+    var dimensions = [];
+    var total = 0;
+    for (var i = 0; i < boxes.length; i++) {
+        dimensions.push(boxes[i].split("x").map(Number));
+    }
+    // console.log(dimensions);
+    for (var i = 0; i < dimensions.length; i++) {
+        var l = dimensions[i][0];
+        var w = dimensions[i][1];
+        var h = dimensions[i][2];
+        var side1 = l * w;
+        var side2 = w * h;
+        var side3 = h * l;
+        var extra = [side1, side2, side3];
+        extra.sort(function(a, b) {
+            return a - b
+        });
+        total += (2 * side1 + 2 * side2 + 2 * side3 + extra[0]);
+        // return box;
+    }
+    return (total);
 };
 
-
-var paperNeeded = wrappingCalculator(['2x3x4','1x1x10'])
-console.log('You need ' + paperNeeded + ' square feet of wrapping paper.')
-exports.wrappingCalculator = wrappingCalculator;
+var paperNeeded = paperCalculator(['2x3x4', '1x1x10']);
+console.log('You need ' + paperNeeded + ' square feet of wrapping paper.');
 exports.paperCalculator = paperCalculator;
-
