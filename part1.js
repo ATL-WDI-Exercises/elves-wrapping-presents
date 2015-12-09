2fe8cb453a87127af8105452fbe871ddca9f1cff
John Kirkley <john@johnkirkley.com>
2015-12-08 21:55:26 -0500

    Cleaned Part 1

--- part1.js
+++ part1.js
@@ -4,23 +4,17 @@
    The boxes argument is assumed to be an array of strings each containing the
    dimensions of a box to be wrapped, for example:
      ['2x3x4', '3x5x8', '1x1x10']
-       var paperNeeded;
-var length, width, height, minSide;
-function paperCalculator(boxes) {
-  for (var i = 0;i < boxes.length; i++ ){
-    var boxDimensions = boxes[i].split('x');
-    length = boxDimensions[0];
-    width = boxDimensions[1];
-    height = boxDimensions[2];
-    minSide = Math.min((length*width),(width*height),(height*length));
-    paperNeeded += (2*length*width) + (2*width*height) + (2*height*length) + minSide;
-    console.log(paperNeeded);
-
-    return paperNeeded;
 */
 function paperCalculator(boxes) {
-  // TODO: return the correct amount of wrapping paper needed.
-  return 0;
-};
-
+  var paperNeeded = 0;
+  for (var i = 0; i < boxes.length; i++) {
+    var boxDimensions = boxes[i].split('x');
+    var length = parseInt(boxDimensions[0]);
+    var width = parseInt(boxDimensions[1]);
+    var height = parseInt(boxDimensions[2]);
+    var minSide = Math.min((length * width), (width * height), (height * length));
+    paperNeeded += (2 * length * width) + (2 * width * height) + (2 * height * length) + minSide;
+}
+  return paperNeeded;
+}
 exports.paperCalculator = paperCalculator;
