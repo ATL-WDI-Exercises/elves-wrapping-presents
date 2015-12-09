8e51c21339ac9abf90a7e58f2a109e6ab3efa0d5
Nathan Epperson <nepperson25@gmail.com>
2015-12-09 06:43:51 -0500

    code test wont work but it works in repl.it

--- part2.js
+++ part2.js
@@ -1,12 +1,22 @@
 /*
    Return the ribbon needed to wrap a box.
    The boxes argument is assumed to be an array of strings each containing the
-   dimensions of a box to be wrapped, for example:
+   dims of a box to be wrapped, for example:
      ['2x3x4', '3x5x8', '1x1x10']
 */
+var totals = [];
+var total = 0;
 function ribbonCalculator(boxes) {
-  // TODO: return the correct amount of ribbon needed.
-  return 0;
-};
+  for (i = 0; i < boxes.length; i++) {
+    var dim = boxes[i].split('x').map(Number);
+    dim.sort();
+    var ribbon = (dim[0] * dim[1] * dim[2]) + ((dim[0] * 2) + (dim[1] * 2));
+    totals.push(ribbon);
+  }
+  for (i = 0; i < totals.length; i++) {
+    total += totals[i];
+  }
+  return total;
+}
 
 exports.ribbonCalculator = ribbonCalculator;
