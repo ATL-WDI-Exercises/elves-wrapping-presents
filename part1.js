a1a833d582187eacf21ac8197621148c087efb29
Anthony Sims Jr <anthony.sims.226@gmail.com>
2015-12-08 20:24:55 -0500

    parts 1 & 2 checkout via jasmine and jscs

--- part1.js
+++ part1.js
@@ -1,25 +1,24 @@
 function paperCalculator(boxes) {
-    var dimensions = [];
-    var total = 0;
-    for (var i = 0; i < boxes.length; i++) {
-        dimensions.push(boxes[i].split("x").map(Number));
-    }
-    // console.log(dimensions);
-    for (var i = 0; i < dimensions.length; i++) {
-        var l = dimensions[i][0];
-        var w = dimensions[i][1];
-        var h = dimensions[i][2];
-        var side1 = l * w;
-        var side2 = w * h;
-        var side3 = h * l;
-        var extra = [side1, side2, side3];
-        extra.sort(function(a, b) {
-            return a - b
-        });
-        total += (2 * side1 + 2 * side2 + 2 * side3 + extra[0]);
-        // return box;
-    }
-    return (total);
+  var dimensions = [];
+  var total = 0;
+  for (var i = 0; i < boxes.length; i++) {
+    dimensions.push(boxes[i].split('x').map(Number));
+  }
+  // console.log(dimensions);
+  for (var i = 0; i < dimensions.length; i++) {
+    var l = dimensions[i][0];
+    var w = dimensions[i][1];
+    var h = dimensions[i][2];
+    var side1 = l * w;
+    var side2 = w * h;
+    var side3 = h * l;
+    var extra = [side1, side2, side3];
+    extra.sort(function(a, b) {
+      return a - b;
+    });
+    total += (2 * side1 + 2 * side2 + 2 * side3 + extra[0]);
+  }
+  return (total);
 };
 
 var paperNeeded = paperCalculator(['2x3x4', '1x1x10']);
--- part2.js
+++ part2.js
@@ -1,12 +1,19 @@
-/*
-   Return the ribbon needed to wrap a box.
-   The boxes argument is assumed to be an array of strings each containing the
-   dimensions of a box to be wrapped, for example:
-     ['2x3x4', '3x5x8', '1x1x10']
-*/
 function ribbonCalculator(boxes) {
-  // TODO: return the correct amount of ribbon needed.
-  return 0;
+  var dimensions = [];
+  var total = 0;
+  for (var i = 0; i < boxes.length; i++) {
+    dimensions.push(boxes[i].split('x').map(Number));
+  }
+  // console.log(dimensions);
+  for (var i = 0; i < dimensions.length; i++) {
+    var l = dimensions[i][0];
+    var w = dimensions[i][1];
+    var h = dimensions[i][2];
+    var ribbon = 2 * l + 2 * w;
+    var bow = l * w * h;
+    total += ribbon + bow;
+  }
+  return (total);
 };
 
 exports.ribbonCalculator = ribbonCalculator;
